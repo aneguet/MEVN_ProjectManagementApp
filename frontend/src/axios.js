2e0dd9a8bb8de0e5154399cf9e4d791b8a1b4e1f
Anna Lopez <annalopezribo@gmail.com>
2022-05-08 23:32:44 +0200

    frontend deploy with backend link

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,2 +1,3 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:4000/api';
+axios.defaults.baseURL = 'https://mevn-project-management-app.herokuapp.com/';
+// axios.defaults.baseURL = 'http://localhost:4000/api';
